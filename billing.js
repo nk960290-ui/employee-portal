c6b0486aa4a551ecd9387b45f6d31e96fdd45440
nk960290-ui <nk960290@gmail.com>
2026-06-19 18:25:17 +0530

    Add files via upload

---
+++ billing.js
@@ -0,0 +1,10 @@
+let product = "Laptop";
+let price = 45000;
+let qty = 2;
+
+let total = price * qty;
+
+console.log("Product : " + product);
+console.log("Price : " + price);
+console.log("Quantity : " + qty);
+console.log("Total : " + total);
