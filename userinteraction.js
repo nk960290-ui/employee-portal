575be1f6d7b83b5cf732cd54f354c26461eb1958
nk960290-ui <nk960290@gmail.com>
2026-06-19 18:27:34 +0530

    Add files via upload

---
+++ userinteraction.js
@@ -0,0 +1,10 @@
+let name = prompt("Enter Employee Name");
+let age = prompt("Enter Employee Age");
+
+let join = confirm("Do you want to join our company?");
+
+if (join) {
+    alert("Welcome " + name);
+} else {
+    alert("Thank You");
+}
