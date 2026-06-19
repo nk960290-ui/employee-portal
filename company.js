b82460ade72eed9a0512b61fcdb3369e6137dc24
nk960290-ui <nk960290@gmail.com>
2026-06-19 16:56:46 +0530

    Add files via upload

---
+++ company.js
@@ -0,0 +1,17 @@
+// Create company object
+const company = {
+    companyName: "it Solutions India",
+    location: "Bengaluru, Karnataka",
+    employees: 250,
+    technologies: ["JavaScript", "React", "Node.js", "Python", "AWS", "Docker"]
+};
+
+// Print company object information
+console.log("=== Company Information ===");
+console.log("Company Name:", company.companyName);
+console.log("Location:", company.location);
+console.log("Number of Employees:", company.employees);
+console.log("Technologies:", company.technologies.join(", "));
+console.log("\nFull Object:", company);
+
+
