db789e314468f41ade17f2d4f5e0a48e4f03e286
nk960290-ui <nk960290@gmail.com>
2026-06-19 15:56:15 +0530

    Add files via upload

---
+++ empolyee.js
@@ -0,0 +1,17 @@
+//Employee Registration System
+
+// Store employee details using let/const
+const employeeName = "Nandhakumar";
+const employeeId = "EMP101";
+const department = "Development";
+const salary = 35000;
+const isPermanent = true;
+
+// Display employee details using template string
+console.log("Employee Details");
+console.log("----------------");
+console.log(`Name : ${employeeName}`);
+console.log(`ID : ${employeeId}`);
+console.log(`Department : ${department}`);
+console.log(`Salary : ${salary}`);
+console.log(`Permanent : ${isPermanent}`);
