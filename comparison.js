026d94084054b561bdf34bedac6a1c67f63a79be
nk960290-ui <nk960290@gmail.com>
2026-06-19 18:26:11 +0530

    Add files via upload

---
+++ comparison.js
@@ -0,0 +1,13 @@
+console.log(10 == "10"); // true
+
+console.log(10 === "10"); // false
+
+console.log(20 != "20"); // false
+
+console.log(20 !== "20"); // true
+
+console.log(5 < 10); // true
+
+console.log(15 >= 20); // false
+
+console.log(100 <= 100); // true
