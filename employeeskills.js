1e20672586475f1e5f244d37931825e76f64fbf2
nk960290-ui <nk960290@gmail.com>
2026-06-19 16:42:23 +0530

    Add files via upload

---
+++ employeeskills.js
@@ -0,0 +1,8 @@
+// Array containing employee skills
+const employeeSkills = ["HTML", "CSS", "JavaScript", "React", "Git", "Node.js"];
+
+// Print the required information
+console.log("First Skill: " + employeeSkills[0]);
+console.log("Third Skill: " + employeeSkills[2]);
+console.log("Last Skill: " + employeeSkills[employeeSkills.length - 1]);
+console.log("Total Skills: " + employeeSkills.length);
